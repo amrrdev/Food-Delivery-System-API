fbf16d9732d4755804bab2d307a725f0b8321408
amrrdev <amrrdev@gmail.com>
2024-08-19 00:25:55 +0300

    Add cookie parse middleware for parsing cookie to request object

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import express from "express";
 import { adminRouter, vendorRouter } from "./routes";
 import { handleGlobalErrors, handleNotFound } from "./middlewares";
+
+import express from "express";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // /admin/vendros
 // /admin/vendros/:id
@@ -13,6 +16,7 @@ app.use("/admin", adminRouter);
 // /vendor/proile
 // /vendor/resetPassword
 
+// restaurant owner
 app.use("/vendor", vendorRouter);
 
 app.use(handleNotFound);
