7036404c99a2f9add0f3f5fdab84a623326a82ee
amrrdev <amrrdev@gmail.com>
2024-08-19 15:53:32 +0300

    feat: add image upload feature for vendors including food images and cover images

--- routes/vendorRoute.ts
+++ routes/vendorRoute.ts
@@ -1,26 +1,40 @@
+import path from "node:path";
+
 import {
   addFood,
   deleteMyProfile,
   getFoods,
   getVendorProfile,
+  updateVendorCoverImage,
   updateVendorProfile,
   updateVendorService,
   vendorLogin,
 } from "../controllers";
-
-import express, { Request, Response, NextFunction } from "express";
 import { checkAuthentication } from "../middlewares";
 
+import express, { Request, Response, NextFunction } from "express";
+import multer from "multer";
+
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, path.join(__dirname, "./../images"));
+  },
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploadImages = multer({ storage }).array("images", 10);
 
 router.route("/login").post(vendorLogin);
 
 router.use(checkAuthentication);
 
 router.route("/profile").get(getVendorProfile).patch(updateVendorProfile).delete(deleteMyProfile);
-
+router.route("/coverImage").patch(uploadImages, updateVendorCoverImage);
 router.route("/service").patch(updateVendorService);
-
-router.route("/foods").get(getFoods).post(addFood);
+router.route("/foods").get(getFoods).post(uploadImages, addFood);
 
 export { router as vendorRouter };
