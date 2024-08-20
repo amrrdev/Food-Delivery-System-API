0efe3a8c44b7066ddd8406301b2c9a04ae2d9fe5
amrrdev <amrrdev@gmail.com>
2024-08-21 02:36:49 +0300

    Define CreateUserInput class and CustomerPayload interface
    - Added CreateUserInput class with validation rules for user creation
    - Defined validation rules for first name, last name, email, password, phone, and address using class-validator
    - Created CustomerPayload interface for user payload with id, email, and erified fields

---
+++ dto/Customer.dto.ts
@@ -0,0 +1,34 @@
+import { IsString, IsEmail, Length, IsNotEmpty } from "class-validator";
+
+export class CreateUserInput {
+  @IsString({ message: "Customer name must be a string" })
+  @IsNotEmpty({ message: "Customer first name is required" })
+  firstName: string;
+
+  @IsString({ message: "Customer name must be a string" })
+  @IsNotEmpty({ message: "Customer last name is required" })
+  lastName: string;
+
+  @IsEmail({}, { message: "Invalid email format" })
+  @IsNotEmpty({ message: "Email is required" })
+  email: string;
+
+  @IsString()
+  @Length(6, 20, { message: "Password must be between 6 and 20 characters" })
+  @IsNotEmpty({ message: "Password is required" })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty({ message: "Phone number is required" })
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty({ message: "address is required" })
+  address: string;
+}
+
+export interface CustomerPayload {
+  id: string;
+  email: string;
+  verified: boolean;
+}
