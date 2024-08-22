784a99fd1455ec713d16f054a6e054b2abe507b9
amrrdev <amrrdev@gmail.com>
2024-08-22 12:22:46 +0300

    feat(routes): add customer profile routes with authentication middleware
    - Implemented customerProfile and customerUpdateProfile routes for retrieving and updating customer profiles.
    - Added checkAuthentication middleware to ensure routes are protected and accessible only to authenticated users.
    - Exported router as customerRoute for integration into the application.

--- routes/customerAuthRoute.ts
+++ routes/customerAuthRoute.ts
@@ -1,11 +1,23 @@
 import express from "express";
-import { customerLogin, customerLogout, customerSignup, customerVerify } from "../controllers";
+import {
+  customerDeleteProfile,
+  customerLogin,
+  customerLogout,
+  customerSignup,
+  customerVerify,
+  sendDeleteProfileOTP,
+} from "../controllers";
+import { checkAuthentication } from "../middlewares";
 
 const router = express.Router();
 
 router.post("/signup", customerSignup);
 router.post("/login", customerLogin);
-router.get("/logout", customerLogout);
 router.patch("/verify/:id", customerVerify);
+
+router.use(checkAuthentication);
+
+router.get("/logout", customerLogout);
+router.route("/delete-me").get(sendDeleteProfileOTP).delete(customerDeleteProfile);
 
 export { router as customerAuthRoute };
