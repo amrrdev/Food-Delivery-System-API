aedeae7a44311fc8b2d1d9baed6ac03ca64affa0
amrrdev <amrrdev@gmail.com>
2024-08-22 12:34:57 +0300

    Add routes and logging middleware to index.ts
    - Added /auth route for customer authentication (customerAuthRoute).
    - Added /customer route for customer-related operations (customerRoute).

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import path from "node:path";
 
-import { adminRouter, vendorRouter, shoppingRouter } from "./routes";
+import {
+  adminRouter,
+  vendorRouter,
+  shoppingRouter,
+  customerAuthRoute,
+  customerRoute,
+} from "./routes";
 import { handleGlobalErrors, handleNotFound } from "./middlewares";
 
 import express from "express";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
 const app = express();
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,6 +25,8 @@ app.use("/admin", adminRouter);
 
 app.use("/vendor", vendorRouter);
 app.use("/shopping", shoppingRouter);
+app.use("/auth", customerAuthRoute);
+app.use("/customer", customerRoute);
 
 app.use(handleNotFound);
 app.use(handleGlobalErrors);
