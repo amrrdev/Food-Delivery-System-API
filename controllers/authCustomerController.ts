559c06a22bad21acc464b77c29da6dbef39f79df
amrrdev <amrrdev@gmail.com>
2024-08-22 12:15:08 +0300

    feat(auth): implement customer login, logout, and profile deletion with OTP verification

--- controllers/authCustomerController.ts
+++ controllers/authCustomerController.ts
@@ -1,31 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import { plainToClass } from "class-transformer";
 import asyncWrapper from "../utility/asyncWrapper";
-import { CreateUserInput, CustomerPayload } from "../dto";
+import { CreateUserInput, CustomerLoginInput, CustomerPayload } from "../dto";
 import { validate } from "class-validator";
 import AppError from "../utility/AppError";
 import { StatusCodes } from "http-status-codes";
 import { Customer } from "../models";
 
-import bcrypt from "bcrypt";
-
-import { attachCookiesToResponse, generateOTP, hashOTP, sendOTPEmail } from "../utility";
+import {
+  attachCookiesToResponse,
+  generateOTP,
+  handleValidationErrors,
+  hashOTP,
+  sendOTPEmail,
+} from "../utility";
 
 export const customerSignup = asyncWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
     const customerInput = plainToClass(CreateUserInput, req.body);
     const errors = await validate(customerInput);
 
-    if (errors.length > 0) {
-      const formattedErrors = errors
-        .map((error) => {
-          const propertyErrors = Object.values(error.constraints || {}).join(", ");
-          return `${error.property}: ${propertyErrors}`;
-        })
-        .join("; ");
-
-      return next(new AppError(`Error in user input: ${formattedErrors}`, StatusCodes.BAD_REQUEST));
-    }
+    handleValidationErrors(errors, next);
 
     const otp = generateOTP();
 
@@ -56,17 +51,15 @@ export const customerSignup = asyncWrapper(
 
 export const customerVerify = asyncWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
-    const customer = await Customer.findById(req.params.id);
+    const customer = await Customer.findById(req.params.id).select("+otp +otpExpiry");
 
     const otp = req.body.otp as string;
 
     if (!customer) {
       return next(new AppError("Customer not found, Please login again", StatusCodes.NOT_FOUND));
     }
-    console.log(customer);
-    console.log("1");
+
     const { hashedOTP, otpExpiry } = hashOTP(otp);
-    console.log("2");
 
     if (hashedOTP !== customer.otp || Date.now() >= customer.otpExpiry) {
       return next(new AppError("Invalid or expired OTP", StatusCodes.BAD_REQUEST));
@@ -88,10 +81,115 @@ export const customerVerify = asyncWrapper(
     });
   }
 );
-
 export const customerLogin = asyncWrapper(
-  async (req: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    const customerInput = plainToClass(CustomerLoginInput, req.body);
+    const errors = await validate(customerInput);
+
+    handleValidationErrors(errors, next);
+
+    const customer = await Customer.findOne({ email: customerInput.email }).select("+password");
+
+    if (
+      !customer ||
+      !(await customer.comparePasswords(customerInput.password, customer.password))
+    ) {
+      return next(new AppError("Incorrect email or password", StatusCodes.NOT_FOUND));
+    }
+
+    if (!customer.verified) {
+      return next(
+        new AppError(
+          `Your account is not verified, please send a get request to /auth/verify/${customer._id}`,
+          StatusCodes.UNAUTHORIZED
+        )
+      );
+    }
+
+    attachCookiesToResponse(res, {
+      id: customer._id as string,
+      email: customer.email,
+      verified: customer.verified,
+    });
+
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      data: { customer },
+    });
+  }
 );
+
 export const customerLogout = asyncWrapper(
-  async (req: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.cookie("token", "logout", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    });
+    res.status(StatusCodes.OK).json({ message: "Customer logged out" });
+  }
+);
+
+// get /delete-me -> otp
+export const sendDeleteProfileOTP = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const customer = req.user;
+
+    if (!customer)
+      return next(new AppError("Customer not found, Please login again", StatusCodes.NOT_FOUND));
+
+    const profile = await Customer.findById(customer.id).select("+otp +otpExpiry");
+
+    if (!profile) {
+      return next(new AppError("There's no customer exist, please login", StatusCodes.NOT_FOUND));
+    }
+    const otp = generateOTP();
+
+    const { hashedOTP, otpExpiry } = hashOTP(otp);
+
+    await sendOTPEmail(profile.email, otp);
+
+    profile.otp = hashedOTP;
+    profile.otpExpiry = otpExpiry;
+
+    await profile.save();
+
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      message: "OTP sent to your email. Please verify",
+    });
+  }
+);
+
+// delete /delete-me with otp
+export const customerDeleteProfile = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const customer = req.user;
+    const otp = req.body.otp;
+    if (!customer)
+      return next(new AppError("Customer not found, Please login again", StatusCodes.NOT_FOUND));
+
+    const profile = await Customer.findById(customer.id).select("+otp + otpExpiry");
+
+    if (!profile) {
+      return next(new AppError("There's no customer exist, please login", StatusCodes.NOT_FOUND));
+    }
+
+    const { hashedOTP, otpExpiry } = hashOTP(otp);
+
+    if (hashedOTP !== profile.otp || Date.now() >= profile.otpExpiry) {
+      return next(new AppError("Invalid or expired OTP", StatusCodes.BAD_REQUEST));
+    }
+
+    await profile.deleteOne();
+
+    res.cookie("token", "logout", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    });
+
+    res.status(StatusCodes.OK).json({
+      status: "success",
+      message: "deleted successfully",
+    });
+  }
 );
