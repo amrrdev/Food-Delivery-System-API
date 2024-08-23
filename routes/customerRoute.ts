b3bf531a5cba1dffb9c5da12b03ed549ab7b9ec8
amrrdev <amrrdev@gmail.com>
2024-08-24 02:00:05 +0300

    feat(routes): add cart and order routes for customers
    - Added /cart route with GET, POST, and DELETE methods to handle retrieving, adding to, and deleting the cart.
    - Introduced /create-order route for creating a new order.
    - Added /orders route to get all orders for a customer.
    - Added /orders/:id route to fetch order details by ID.

--- routes/customerRoute.ts
+++ routes/customerRoute.ts
@@ -1,5 +1,12 @@
 import express from "express";
-import { customerProfile, customerUpdateProfile } from "../controllers";
+import {
+  addToCart,
+  customerProfile,
+  customerUpdateProfile,
+  deleteCart,
+  getCart,
+} from "../controllers";
+import { createOrder, getAllOrders, getOrderById } from "../controllers/orderController";
 import { checkAuthentication } from "../middlewares";
 
 const router = express.Router();
@@ -7,5 +14,11 @@ const router = express.Router();
 router.use(checkAuthentication);
 
 router.route("/profile").get(customerProfile).patch(customerUpdateProfile);
+
+router.route("/cart").get(getCart).post(addToCart).delete(deleteCart);
+
+router.post("/create-order", createOrder);
+router.get("/orders", getAllOrders);
+router.get("/orders/:id", getOrderById);
 
 export { router as customerRoute };
