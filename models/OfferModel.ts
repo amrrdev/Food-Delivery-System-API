6a0e667577520ea2343554c35d1c8aff60fa8c8e
amrrdev <amrrdev@gmail.com>
2024-08-25 00:13:24 +0300

    Add Offer model with schema and interface
    - Define OfferDocument interface to represent offer documents in MongoDB.
    - Implement OfferSchema with validation for offer fields including type, vendors, title, description, discount percentage, expiration date, pincode, and activation status.
    - Configure schema to remove the __v field from JSON output and add timestamps for creation and update.

---
+++ models/OfferModel.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document } from "mongoose";
+
+export interface OfferDocument extends Document {
+  offerType: string; //Vendor // Generic
+  vendors: any[];
+  title: string;
+  description: string;
+  discountPercentage: number; //min order amount should 300 EG
+  expirationDate: Date;
+  pincode: string; // OFFER WITHIN SPECIFIC AREA
+  isActive: boolean;
+}
+
+const OfferSchema = new mongoose.Schema<OfferDocument>(
+  {
+    offerType: { type: String, required: true },
+    vendors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vendor",
+        required: true,
+      },
+    ],
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    discountPercentage: {
+      type: Number,
+      required: [true, "Discount percentage is required"],
+      min: [0, "Discount percentage cannot be less than 0"],
+      max: [100, "Discount percentage cannot exceed 100"],
+    },
+    expirationDate: {
+      type: Date,
+      required: [true, "Expiration date is required"],
+    },
+    pincode: { type: String, required: true },
+    isActive: Boolean,
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: true },
+    toJSON: {
+      transform(doc, returnJSObject) {
+        delete returnJSObject.__v;
+      },
+    },
+  }
+);
+
+export const Offer = mongoose.model<OfferDocument>("Offer", OfferSchema);
