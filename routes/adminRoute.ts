ae692167f7edf15fd4b52cac33c3fbcb6a910a94
amrrdev <amrrdev@gmail.com>
2024-08-25 00:18:48 +0300

    Add routes for fetching transactions and transaction by ID

--- routes/adminRoute.ts
+++ routes/adminRoute.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import { createVendor, deleteVendor, getAllVendors, getVendorById } from "../controllers";
+import {
+  createVendor,
+  deleteVendor,
+  getAlltransaction,
+  getAllVendors,
+  getTransactionById,
+  getVendorById,
+} from "../controllers";
 
 const router = express.Router();
 
 router.route("/vendors").post(createVendor).get(getAllVendors);
 router.route("/vendors/:id").get(getVendorById).delete(deleteVendor);
+router.route("/transactions").get(getAlltransaction);
+router.route("/transaction/:id").get(getTransactionById);
 
 export { router as adminRouter };
